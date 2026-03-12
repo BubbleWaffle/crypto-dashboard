bcb2c732df431bac73736a9a01ac8ae0cddf0794
BubbleWaffle <hunter13cie@gmail.com>
2026-03-12 15:12:11 +0100

    Cleaning code

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -4,24 +4,24 @@ import TotalVolumesChart from "./components/TotalVolumesChart";
 import { useCoinChart } from "./hooks/useCoinChart";
 import TimeRangeSelector from "./components/TimeRangeSelector";
 import CurrencyRangeSelector from "./components/CurrencyRangeSelector";
+import { Box } from "@mui/material";
 
 type DashboardProps = {
     coinId: string;
 };
 
 function Dashboard({ coinId }: DashboardProps) {
-    console.log(coinId);
     const [days, setDays] = useState(30);
     const [currency, setCurrency] = useState("usd");
     const { price, marketCap, volume, loading, error } = useCoinChart({ coinId, currency, days });
     return(
-        <>
-            <h1>Bitcoin chart</h1>
+        <Box sx={{ mt: 8 }}>
+            <h1>{coinId}</h1>
             <TimeRangeSelector value={days} onChange={setDays} />
             <CurrencyRangeSelector value={currency} onChange={setCurrency} />
             <CoinChart data={price} loading={loading} error={error} />
             <TotalVolumesChart data={volume} loading={loading} error={error} />
-        </>
+        </Box>
     );
 }
 
